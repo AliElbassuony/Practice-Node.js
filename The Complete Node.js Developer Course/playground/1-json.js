28476e8cd1863a3ae5fd9b5a5a3d44b7e2bd9a9e
AliElbassuony <alimohamed19499@gmail.com>
2022-04-23 23:56:30 +0200

    Storing Data With JSON

---
+++ The Complete Node.js Developer Course/playground/1-json.js
@@ -0,0 +1,46 @@
+const fs = require('fs') 
+
+// const book = {
+//     title: 'The Road To Be Hacker!',
+//     author: 'ALi'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json',bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+
+// console.log(data.author)
+
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+
+data.name = "Ali"
+data.planet = "Earth"
+data.age = 23
+
+
+const newData = JSON.stringify(data)
+
+fs.writeFileSync('1-json.json',newData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
