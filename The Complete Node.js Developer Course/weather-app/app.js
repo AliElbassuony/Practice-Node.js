f912f62530d887b62733fc4738daa6a24c3e8c3c
AliElbassuony <alimohamed19499@gmail.com>
2022-05-25 21:23:53 +0200

    HTTP Requests

--- The Complete Node.js Developer Course/weather-app/app.js
+++ The Complete Node.js Developer Course/weather-app/app.js
@@ -1,7 +1,11 @@
-console.log('Starting')
+const requet = require('postman-request')
 
-setTimeout(() => {
-    console.log('Wating 2 seconds')
-},2000)
+const url = 'http://api.weatherstack.com/current?access_key=2a61d4cf7905485e11184ac974b00f52&query=37,40&units=f'
 
-console.log('Stoping')
+requet({url: url, json: true}, (error, response) => {
+    
+    const temperature = response.body.current.temperature;
+    const feelsLike = response.body.current.feelslike;
+    const weatherdesc = response.body.current.weather_descriptions[0];
+    console.log(`${weatherdesc}. It is currently ${temperature} degrees out. It's feels like ${feelsLike} degrees.`)
+})
