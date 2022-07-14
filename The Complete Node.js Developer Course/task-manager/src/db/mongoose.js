9451b503ce2731c83ece85b1e975d1eba799a54f
AliElbassuony <alimohamed19499@gmail.com>
2022-07-14 23:37:40 +0200

    Validation and Sanitization

--- The Complete Node.js Developer Course/task-manager/src/db/mongoose.js
+++ The Complete Node.js Developer Course/task-manager/src/db/mongoose.js
@@ -1,20 +1,52 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
-// const User = mongoose.model('User',{
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const User = mongoose.model('User',{
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error(`Password can't contain password`)
+            }   
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error(`Email is invalid!`)
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error(`Age must be a positive number!`)
+            }
+        }
+    }
+})
 
-// const me = new User({
-//     name: 'Ali',
-//     age: 23
-// })
+const me = new User({
+    name: ' Ayat ',
+    email: ' Ayat@ali.IO ',
+    password: 'passw0rd'
+})
 
 // me.save().then(() => {
 //     console.log(me)
@@ -22,16 +54,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 const Task = mongoose.model('Task',{
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   } ,
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false 
   }
 })
 
 const task1 = new Task({
     description: 'Never Give UP',
-    completed: false
+    // completed: false
 })
 
 task1.save().then(() => console.log(task1)).catch((error) => console.log(error))
